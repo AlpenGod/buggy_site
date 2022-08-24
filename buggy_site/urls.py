c02660ce6668db32536ef5d32e47d1afbb0d9b29
AlpenGod <dominob1999@gmail.com>
2022-08-24 13:48:09 -0400

    static urlpatter added

--- buggy_site/urls.py
+++ buggy_site/urls.py
@@ -54,3 +54,4 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
